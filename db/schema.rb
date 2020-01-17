e349e56fa36462910930d40831afa50016d84bda
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-01-17 13:12:12 -0800

    fix reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_193615) do
+ActiveRecord::Schema.define(version: 2020_01_17_210631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_01_17_193615) do
     t.string "author"
     t.string "content_body"
     t.integer "rating"
+    t.integer "product_id"
   end
 
 end
